d3c3de03be9a65a8f04a9c0912d5141edf6e0f80
louksky <37488746+louksky@users.noreply.github.com>
2020-11-24 15:03:57 +0200

    Add files via upload

---
+++ enc12/enc12/Form1.Designer.cs
@@ -0,0 +1,160 @@
+﻿namespace enc12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pass_origin = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.contextMenuStrip2 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.pass_after = new System.Windows.Forms.TextBox();
+            this.ENC = new System.Windows.Forms.Button();
+            this.DEC = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Heset = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // pass_origin
+            // 
+            this.pass_origin.Location = new System.Drawing.Point(81, 34);
+            this.pass_origin.Name = "pass_origin";
+            this.pass_origin.Size = new System.Drawing.Size(100, 20);
+            this.pass_origin.TabIndex = 0;
+            this.pass_origin.Text = "password123";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // contextMenuStrip2
+            // 
+            this.contextMenuStrip2.Name = "contextMenuStrip2";
+            this.contextMenuStrip2.Size = new System.Drawing.Size(61, 4);
+            // 
+            // pass_after
+            // 
+            this.pass_after.Location = new System.Drawing.Point(81, 63);
+            this.pass_after.Name = "pass_after";
+            this.pass_after.Size = new System.Drawing.Size(100, 20);
+            this.pass_after.TabIndex = 4;
+            // 
+            // ENC
+            // 
+            this.ENC.Location = new System.Drawing.Point(145, 158);
+            this.ENC.Name = "ENC";
+            this.ENC.Size = new System.Drawing.Size(127, 91);
+            this.ENC.TabIndex = 5;
+            this.ENC.Text = "ENC";
+            this.ENC.UseVisualStyleBackColor = true;
+            this.ENC.Click += new System.EventHandler(this.ENC_Click);
+            // 
+            // DEC
+            // 
+            this.DEC.Location = new System.Drawing.Point(12, 158);
+            this.DEC.Name = "DEC";
+            this.DEC.Size = new System.Drawing.Size(127, 91);
+            this.DEC.TabIndex = 6;
+            this.DEC.Text = "DEC";
+            this.DEC.UseVisualStyleBackColor = true;
+            this.DEC.Click += new System.EventHandler(this.DEC_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(231, 34);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "סיסמא";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(190, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 8;
+            this.label2.Text = "סיסמא מוצפנת";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(239, 105);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 13);
+            this.label3.TabIndex = 9;
+            this.label3.Text = "היסט";
+            // 
+            // Heset
+            // 
+            this.Heset.Location = new System.Drawing.Point(81, 98);
+            this.Heset.Name = "Heset";
+            this.Heset.Size = new System.Drawing.Size(100, 20);
+            this.Heset.TabIndex = 10;
+            this.Heset.Text = "20";
+            this.Heset.TextChanged += new System.EventHandler(this.update_eset);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.Heset);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.DEC);
+            this.Controls.Add(this.ENC);
+            this.Controls.Add(this.pass_after);
+            this.Controls.Add(this.pass_origin);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.TextChanged += new System.EventHandler(this.update_eset);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox pass_origin;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip2;
+        private System.Windows.Forms.TextBox pass_after;
+        private System.Windows.Forms.Button ENC;
+        private System.Windows.Forms.Button DEC;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Heset;
+    }
+}
+
---
+++ enc12/enc12/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace enc12
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private int Eset = 4;
+        public void Enc_Password()
+        {
+            int eset = Eset;
+            string str_pass_origin = this.pass_origin.Text;
+            string str_pass_enc = "";
+            for(int i =0; i < str_pass_origin.Length; i++)
+            {
+                char ch = str_pass_origin[i];
+                int res = (((int)(ch) + eset))% 127;
+                char ch_enc = (char)(res);
+                str_pass_enc += "" + ch_enc;
+            }
+            pass_after.Text = str_pass_enc;
+        }
+        public void Dec_Password()
+        {
+            int eset = Eset;
+            string str_pass_enc = this.pass_after.Text;
+            string str_pass_origin = "";
+
+            for(int i = 0; i < str_pass_enc.Length; i++)
+            {
+                char ch_enc = str_pass_enc[i];
+                int res = ((int)ch_enc - eset) % 127;
+                if(res < 0)
+                {
+                    res = 127 - res;
+                }
+                char ch = (char)res;
+                str_pass_origin += "" + ch;
+            }
+            //חדששששששששששש
+            MessageBox.Show(str_pass_origin);
+
+       
+        }
+        private void ENC_Click(object sender, EventArgs e)
+        {
+            Enc_Password();
+        }
+
+        private void DEC_Click(object sender, EventArgs e)
+        {
+            Dec_Password();
+        }
+
+        private void update_eset(object sender, EventArgs e)
+        {
+            try
+            {
+                this.Eset = int.Parse(Heset.Text);
+            }
+            catch
+            {
+                MessageBox.Show("הזנת ערכים לא תקינים במערכת ");
+                Heset.Text = ""+this.Eset;
+                return ;
+            }
+          
+            
+        }
+    }
+}
